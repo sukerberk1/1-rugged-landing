61bedaf4560b2721191aee3f367be8c5816f0354
sukerberk1 <szymon.l3232@gmail.com>
2025-02-08 00:21:26 +0100

    Add swiper hero section

---
+++ swiper/swiper-simple.js
@@ -0,0 +1,12 @@
+
+const swiper = new Swiper(".mySwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true
+  }
+});
