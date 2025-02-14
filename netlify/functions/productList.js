edc880575aaff4e980ff1b74ab417145faee3bea
sukerberk1 <szymon.l3232@gmail.com>
2025-02-14 21:27:59 +0100

    Add product categories

--- netlify/functions/productList.js
+++ netlify/functions/productList.js
@@ -1,15 +1,21 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const deviceCategories = [
+    { categoryName: "Wytrzymałe Tablety", categoryDevices: ["M14A", "M14M", "M10A", "M10T"] },
+    { categoryName: "Wytrzymałe NoteBooki", categoryDevices: ["N14A", "N14M", "N15A", "N15M"] },
+    { categoryName: "Komputery Panel PC", categoryDevices: ["P21A"] },
+    { categoryName: "Wytrzymałe urządzenia Handheld", categoryDevices: ["H68T"] },
+];
+
 exports.handler = async (event) => {
     try {
-        const targetUrl = "https://www.onerugged.com/"; // Change this to the site you want to scrape
+        const targetUrl = "https://www.onerugged.com/";
         const { data } = await axios.get(targetUrl);
 
         const $ = cheerio.load(data);
-        const title = $("div.nbame").first().text(); // Extract first <h1> as example
 
-        let htmlResponse = "";
+        const allDevices = [];
 
         $(".li2 .navboxItem .blx_list ul li").each((index, element) => {
             const title = $(element).find(".nbame").text().trim();
@@ -17,18 +23,35 @@ exports.handler = async (event) => {
             const relativeLink = $(element).find("a").attr("href");
             const productLink = `https://www.onerugged.com${relativeLink}`; // Convert relative to absolute URL
             const fullImageUrl = `https://www.onerugged.com${imageUrl}`; // Convert image path to absolute
+            
+            allDevices.push({ title, fullImageUrl, productLink });
+        });
 
+        let htmlResponse = "";
+
+        deviceCategories.forEach((category) => {
             htmlResponse += `
+            <div style="grid-column: 1/-1;">
+                <h2 class="font-semibold text-2xl mb-4">${category.categoryName}</h2>
+                <hr/>
+            </div>
+            `;
+
+            allDevices.filter(o => category.categoryDevices.includes(o.title)).forEach((device) => {
+                htmlResponse += `
                 <div class="bg-white shadow-lg">
-                    <img src="${fullImageUrl}" alt="${title}"
+                    <img src="${device.fullImageUrl}" alt="${device.title}"
                         class="w-full h-40 object-cover mb-4">
                     <div class="p-6">
-                        <h3 class="text-2xl font-semibold mb-4">${title}</h3>
-                        <a href="/product?url=${productLink}" class="text-yellow-700 font-semibold hover:underline">Dowiedz się więcej</a>
+                        <h3 class="text-2xl font-semibold mb-4">${device.title}</h3>
+                        <a href="/product?url=${device.productLink}" class="text-yellow-700 font-semibold hover:underline">Dowiedz się więcej</a>
                     </div>
                 </div>
             `;
+            });
         });
+
+        
 
         return {
             statusCode: 200,
