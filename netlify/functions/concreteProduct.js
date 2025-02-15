ff84149d434300a048fb8041a2142f922d446918
sukerberk1 <szymon.l3232@gmail.com>
2025-02-15 11:58:16 +0100

    Add service mail

--- netlify/functions/concreteProduct.js
+++ netlify/functions/concreteProduct.js
@@ -22,10 +22,20 @@ exports.handler = async (event) => {
         const mainImgUrl = "https://onerugged.com" + $(".productSwiper").find(".images img").attr("src");
 
         htmlResponse += `
-        <section class="relative w-screen h-64">
+        <section class="relative w-screen h-64" id="hero">
+            <div class="absolute top-0 left-0 w-full bg-yellow-500 bg-opacity-50 z-50 text-white">
+                <div class="p-3 w-full flex justify-around">
+                    <div class="inline-flex items-center gap-2">
+                        <img src="./assets/svg/mail.svg" alt="Mail" class="w-6 inline-block" /> Obsługa: <a href="mailto:serwis@1-rugged.pl">serwis@1-rugged.pl</a>
+                    </div>
+                    <div class="inline-flex items-center gap-2">
+                        <img src="./assets/svg/telephone.svg" alt="Mail" class="w-6 inline-block" /> +48 123 456 789
+                    </div>
+                </div>
+            </div>
             <img src="${mainImgUrl}" alt="hero-bg"
                 class="w-full h-full object-cover">
-            <div class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 backdrop-blur-sm">
+            <div class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-30 backdrop-blur-sm">
                 <div class="flex flex-col items-center justify-center h-full">
                     <div class="text-center max-w-4xl">
                         <h1 class="text-4xl font-semibold text-white">${title}</h1>
