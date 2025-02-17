d36153141a402019f2f49a8a70f7144297b4416c
sukerberk1 <szymon.l3232@gmail.com>
2025-02-17 23:12:56 +0100

    Add corrections - dsahed naming fix

--- netlify/functions/concreteProduct.js
+++ netlify/functions/concreteProduct.js
@@ -170,7 +170,7 @@ exports.handler = async (event) => {
                 <div class="flex flex-wrap items-center md:justify-between justify-center">
                     <div class="w-full md:w-4/12 px-4 mx-auto text-center">
                         <div class="text-sm text-blueGray-500 font-semibold py-1">
-                            Copyright © <span id="get-current-year">2025</span> LITEKO / 1-rugged
+                            Copyright © <span id="get-current-year">2025</span> LITEKO / 1rugged
                         </div>
                     </div>
                 </div>
